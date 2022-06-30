4c980ac5a0e661b32d127b32ef9f3f826926c645
Evan Boehs <evan@boehs.org>
2022-06-30 13:56:31 -0400

    fix: migrate left to route to fix bug in solid-start
    thanks to nksaraf for help debugging and the coming soon fix in solid-start

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,18 +1,41 @@
-import { ErrorBoundary } from "solid-start/error-boundary";
-import Left from "~/components/Main/l/Left";
-import Right from "~/components/Main/r/Right";
-import { CollectionProvider } from "~/components/Main/shared/collection";
 import "./index.css";
+import "~/components/Main/l/Left.scss"
+import "~/components/Button.scss"
+import Right from "~/components/Main/r/Right";
+import ListItem from "~/components/Main/l/ListItem";
+import { CollectionProvider } from "~/components/Main/shared/collection";
+import { ErrorBoundary } from "solid-start/error-boundary";
+import { createServerResource } from 'solid-start/server'
+import { For, Show } from "solid-js";
+import knex from "../../../db/knex"
 
 export default function Home() {
+  const data = createServerResource(async () => {
+    const collections = await knex('collection').select().then(collections => {
+      return collections || []
+    })
+
+    return collections
+  })
+
   return (
     <main>
       <CollectionProvider collection={false}>
         <ErrorBoundary>
-          <Left/>
+          <div id="l">
+            <div class="lToolbar">
+              <button class="but">📁</button>
+              <button class="but">➕</button>
+            </div>
+            <Show when={data()}>
+              <For each={data()}>
+                {collection => <ListItem title={collection.name}></ListItem>}
+              </For>
+            </Show>
+          </div>
         </ErrorBoundary>
         <ErrorBoundary>
-          <Right/>
+          <Right />
         </ErrorBoundary>
       </CollectionProvider>
     </main>
