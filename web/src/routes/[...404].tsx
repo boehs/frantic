ca27d5b60652a9c5f7f70379593061226e44a455
Evan Boehs <evan@boehs.org>
2022-07-01 21:16:51 -0400

    feat: make 404 page slightly better

--- web/src/routes/[...404].tsx
+++ web/src/routes/[...404].tsx
@@ -3,14 +3,10 @@ import { Link } from "solid-app-router";
 export default function NotFound() {
   return (
     <main>
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
+      <div id="l">
+        <h1>Page Not Found</h1>
+        <p>you could always <a href="/">go home</a></p>
+      </div>
     </main>
   );
 }
